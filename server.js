c27208102ffc8dc55b017c553925b3ab83940ae3
Norma Cruz <nbmc1991@gmail.com>
2021-02-19 02:18:47 -0500

    server on

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const mongoose=require('mongoose');
-const routs=require('./routes');//folder needs to be created, with index file
+import express from 'express';
+import mongoose from 'mongoose';
 
-const express=require('express');
-const app=express();
-const PORT=process.env.PORT||3453;
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
-}
-app.use(routs);
+//app config
+const app= express();
+const PORT= process.env.PORT || 7777;
 
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/DevMeet');
+//middleware
 
-app.listen(PORT,()=>{
-    console.log(`🪐==>Data travel activated via portal: ${PORT}`)
-});
+//db config
+
+
+//api endpoint
+app.get('/', (req, res) => res.status(200).send('Hello Clever Programmers'));
+
+
+//listener
+app.listen(PORT, () => console.log(`🌎  ==> http://localhost:${PORT}`));
