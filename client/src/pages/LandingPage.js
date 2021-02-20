de9c6bfd3fec9a4bb526f7d07a55d05ad625c0da
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-19 19:56:22 -0500

    tentative configuration

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'//add bootstrap to html template
+import LoginBtn from '../components/loginbtn';
+import CreateAccountBtn from '../components/createaccntbtn';
+
+const LandingPage=()=>{
+    return(
+        <Container>//bootstrap or materialize component
+            
+        </Container>
+    )
+}
