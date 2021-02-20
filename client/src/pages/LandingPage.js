592f0e1244c25a18e28d35be98304bbd5df39205
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-19 23:07:09 -0500

    tentative configuration

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,11 +1,31 @@
-import React from 'react'//add bootstrap to html template
-import LoginBtn from '../components/loginbtn';
-import CreateAccountBtn from '../components/createaccntbtn';
+import {makeStyles, ThemeProvider} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar'
 
-const LandingPage=()=>{
+const useStyles=makeStyles((them)=>({
+    root:{
+        flexGrow:1,
+    },
+    paper:{
+        padding:ThemeProvider.spacing(2),
+        textAlign:'center',
+        color:ThemeProvider.palette.text.secondary,
+    },
+}));
+
+export default function FullWidthGrid(){
+    const classes=useStyles();
+
     return(
-        <Container>//bootstrap or materialize component
-            
-        </Container>
+        <div className={classes.root}>
+            <Grid container direction='row-reverse' justify='space-around' alignitems='baseline' spacing={3}>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>
+                        <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' className={classes.large}/>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
