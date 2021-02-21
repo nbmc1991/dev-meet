dd6ad822549b9d528aa9c748835d6366b330386a
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-21 15:36:09 -0500

    tentative stack configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import FullWidthGrid from './pages/LandingPage';
+import LandingPage from './pages/LandingPage';
+import PrimarySearchAppBar from './components/AppBar';
 
 function App() {
   return(
     <div>
-      <FullWidthGrid/>
+      <PrimarySearchAppBar/>
+      <LandingPage/>
     </div>
   );
 }
