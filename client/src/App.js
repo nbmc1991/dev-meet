1d8ce2d7dcbec96ac1249254a2887b9dd652ef38
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-21 18:42:28 -0500

    trouble shooting client stack

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
 import PrimarySearchAppBar from './components/AppBar';
+import SimplePaper from './pages/LandingPage';
 
 function App() {
   return(
     <div>
-      <LandingPage/>
       <PrimarySearchAppBar/>
+      <SimplePaper/>
     </div>
   );
 }
