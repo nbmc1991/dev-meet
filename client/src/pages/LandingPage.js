dcdb90ad7a08bb72fda9da533ffaa2b85026d347
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-21 18:45:08 -0500

    updated ui

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 
-const LandingPage=()=>{
+const useStyles=makeStyles(theme=>({
+    root:{
+        display:'flex',
+        flexWrap:'wrap',
+        '&>*':{
+            margin:theme.spacing(1),
+            width:theme.spacing(16),
+            height:theme.spacing(16),
+        },
+    },
+}));
+
+export default function SimplePaper(){
+    const classes=useStyles();
+
     return(
-        <Paper elevation={3}>
-            <h3>da Vjncj dissonance...</h3>
-        </Paper>
+        <div className={classes.root}>
+            <Paper variant='outlined' elevation={0}/>
+            <Paper/>
+            <Paper elevation={3}/>                
+        </div>
     )
 }
-export default LandingPage;
