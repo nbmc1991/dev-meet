5a1febe8eee53cf2a3ca63cd92284c87f17934a9
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-22 06:58:04 -0500

    tentive display stack

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-// import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import PrimarySearchAppBar from './components/AppBar';
-import SimplePaper from './pages/LandingPage';
+import React,{useContext,useEffect} from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import SimpleContainer from './components/pages/LandingPage';
+import {Store} from './store';
 
-function App() {
+const App=()=>{
+  const {dispatch}=useContext(Store);//(store) should fill this spot
+
+  useEffect(()=>{
+    if(localStorage.jwtToken){
+      const token=localStorage.jwtToken;
+      // const decoded=jwt_decode(token);
+      const currentTime=Date.now()/1000;
+    }
+  },[dispatch]);
+
   return(
-    <div>
-      <PrimarySearchAppBar/>
-      <SimplePaper/>
-    </div>
+    <Router>
+      <div className='App'>
+        <Route exact path='/' component={SimpleContainer}/>        
+        <Switch>
+          {/* <PrivateRoute exact path='profilepage' component={ProfilePage}/> */}
+        </Switch>
+      </div>      
+    </Router>
   );
 }
-
 export default App;
