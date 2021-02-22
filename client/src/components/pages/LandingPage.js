01fd91bb0b305d195db5d205277d3b5e297b66c0
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-22 06:56:27 -0500

    tentative ui/ux for landing page

---
+++ client/src/components/pages/LandingPage.js
@@ -0,0 +1,19 @@
+import React,{useContext} from 'react';
+import {Store} from '../../store';//this will be filled with functions for capturing client data
+import {Link} from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+export default function SimpleContainer(){
+    const {state}=useContext(Store);//client info made ready as needed/user data?
+    // console.log({state,props});
+    return(
+        <React.Fragment>
+            <CssBaseline/>
+            <Container maxWidth='sm'>
+                <Typography component='div' style={{backgroundColor:'#pink',height:'100vh' }}/>               
+            </Container>
+        </React.Fragment>
+    )
+};
