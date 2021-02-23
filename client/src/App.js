94e2a1dc47b25919cc9f50f2c33cdabdb5f595be
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-23 10:51:26 -0500

    tentative configuration

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import React,{useContext,useEffect} from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import SimpleContainer from './components/pages/LandingPage';
 import {Store} from './store';
+import PrimarySearchAppBar from '@material-ui/core/AppBar'
+import Navbar from './components/partials/NavBar'
 
 const App=()=>{
-  const {dispatch}=useContext(Store);//(store) should fill this spot
+  // const {dispatch}=useContext();//(store) should fill this spot
 
   useEffect(()=>{
     if(localStorage.jwtToken){
@@ -12,13 +14,14 @@ const App=()=>{
       // const decoded=jwt_decode(token);
       const currentTime=Date.now()/1000;
     }
-  },[dispatch]);
+  },[]);//'dispatch' fills the array
 
   return(
     <Router>
       <div className='App'>
-        <Route exact path='/' component={SimpleContainer}/>
-        <SimpleContainer/>      
+        <Navbar/>
+        {/* <PrimarySearchAppBar/>tentative */}
+        <Route exact path='/' component={SimpleContainer}/>              
         <Switch>
           {/* <PrivateRoute exact path='profilepage' component={ProfilePage}/> */}
         </Switch>
