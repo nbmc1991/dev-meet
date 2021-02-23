1f61d8ba955ddbb2453ba4d7f8b7610897a82e7f
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-23 10:49:41 -0500

    tentative configuration

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import React,{useContext} from 'react';
-import {Store} from '../../store';//this will be filled with functions for capturing client data
+// import {Store} from '../../store';//this will be filled with functions for capturing client data
 import {Link} from 'react-router-dom';
+
 // import CssBaseline from '@material-ui/core/CssBaseline';
 // import Typography from '@material-ui/core/Typography';
 // import Container from '@material-ui/core/Container';
@@ -19,19 +20,42 @@ import {Link} from 'react-router-dom';
 // };
 
 const LandingPage=props=>{
-    const {state}=useContext(store);
-    console.log({state,props});
+    // const {state}=useContext()//(store) was temporarily removed from the useContext()
+    // console.log({state,props});
 
     return(
         <div className='container valign-wrapper' style={{height:'75vh'}}>
-            <div>
-                <div>
+            <div className='row'>
+                <div className='col s12 center-align'>
                     <h4>
-                        
+                        <b>Meet</b> the <b>Dev</b> of your dreams... 
                     </h4>
+                    <p className='flow-text grey-text text-darken-1'>
+                        The perfect place for to build lasting connections + stellar applications
+                    </p>
+                    <br/>
+                    <div className="col s6">
+                        <Link to="/register" className="btn btn-large waves-effect waves-light hoverable blue accent-3" style={{
+                        width: '140px',
+                        borderRadius: '3px',
+                        letterSpacing: '1.5px',
+                        }}>
+                        Register
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/login" className="btn btn-large btn-flat waves-effect white black-text" style={{
+                        width: '140px',
+                        borderRadius: '3px',
+                        letterSpacing: '1.5px',
+                        }}>
+                        Login
+                        </Link>
+                    </div>
                 </div>
             </div>
 
         </div>
     )
-}
+};
+export default LandingPage;
