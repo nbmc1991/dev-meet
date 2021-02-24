eaeb980b96597557ba8640950f23eda2c531a9a1
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-24 10:06:06 -0500

    page logic configured/storing user input

---
+++ client/src/components/pages/Register.js
@@ -0,0 +1,35 @@
+import React,{useContext,useEffect,useRef} from 'react';
+import {Link} from 'react-router-dom';
+// import {Store} from '../../store';
+// import {registerUser,SetErrors} from '../../store/actions/authActions';
+import classnames from 'classnames';
+
+const Register=props=>{
+    const {state,dispatch}=useContext(Store);
+    const errors=state.error;
+    const nameRef=useRef();
+    const emailRef=useRef();
+    const passwordRef=useRef();
+    const password2Ref=useRef();
+
+    useEffect(()=>{
+        if(state.auth.isAuthenticated)
+            props.history.push('/homepage');
+    },[state,props]);
+
+    const onSubmit=ux=>{
+        ux.preventDefault();
+        dispatch(setErrors({response:{data:{} }}));
+
+        const userData={
+            name:nameRef.current.value,
+            email:emailRef.current.value,
+            password:passwordRef.current.value,
+            password2:password2Ref.current.value,
+        };
+        registerUser(userData,props.history)(dispatch);
+    }
+    return(
+        
+    )
+}
