7cecb4fc2ae533839a4a2ba9c877743ebb6c2915
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-23 21:22:47 -0500

    authentication

---
+++ client/src/components/iScan/iScan.js
@@ -0,0 +1,9 @@
+import React,{useContext} from 'react';
+import { Component } from 'react';
+import {Rout,Redirect} from 'react-router-dom';
+import {Store} from '../../store';
+
+const iScan=({component:Component,...rest})=>{
+    const {state}=useContext(Store);
+    return <Route {...rest} render={props=>state.auth.isAuthenticated===true ? (<Component{...props}/>):(<Redirect to='/hompage')}
+}
