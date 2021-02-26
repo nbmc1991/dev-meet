7c08f21d9ba9413b805e12b106b38fa5d8703222
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-26 14:00:13 -0500

    authentication underway, brought needed imports

---
+++ client/src/store/actions/authActions.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+import setAuthToken from '../../setAuthToken';//jwt-decode comes into play
+import jwt_decode from 'jwt-decode';//bit coin mining
+import {Get_ERRORS,SET_CURRENT_USER,USER_LOADING} from './'
