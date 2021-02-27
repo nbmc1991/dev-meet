8ea75bbe0f9c30bce954142abe3d7a89e16c6a92
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-27 00:52:55 -0500

    setting stage for styling/theming

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React,{useContext,useEffect} from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
-// import {Store} from './store';
+import {Store} from './store';
 import NavBar from './components/partials/NavBar';
 import LandingPage from './components/pages/LandingPage'
 import HomePage from './components/pages/HomePage';
@@ -10,15 +10,15 @@ import Login from './components/pages/Login';
 import SecureScan from './components/authentication/SecureScan';
 
 const App=()=>{
-  // const {dispatch}=useContext();//(store) should fill this spot
+  const {dispatch}=useContext(Store);
 
   useEffect(()=>{
     if(localStorage.jwtToken){
       const token=localStorage.jwtToken;
-      // const decoded=jwt_decode(token);
+      const decoded=jwt_decode(token);
       const currentTime=Date.now()/1000;
     }
-  },[]);//'dispatch' fills the array
+  },[dispatch]);
 
   return(
     <Router>
