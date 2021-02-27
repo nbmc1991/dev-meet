07707ce363dea8ebe5747fe8043d45237e447d92
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-26 21:40:25 -0500

    minor update/notes to self

--- client/src/components/authentication/SecureScan.js
+++ client/src/components/authentication/SecureScan.js
@@ -1,12 +1,11 @@
 import React,{useContext} from 'react';
-import { Component } from 'react';
+import { Component } from 'react';//what benefit does it have to make this a class based function?
 import {Route,Redirect} from 'react-router-dom';
-// import {Store} from '../../store';
+import {Store} from '../../store';
 
+//may YOU please explain '...rest' syntax & Component
 const SecureScan=({component:Component,...rest})=>{
-    // const {state}=useContext();//fill useContext() with 'Store'
-    // return <Route {...rest} render={props=>state.auth.isAuthenticated===true ? (<Component{...props}/>):(<Redirect to='/hompage'/>)}/>
-    console.error('this is engineering');
-    console.log('alas, purposeful placement')
+    const {state}=useContext(Store);
+        return <Route {...rest} render={props=>state.auth.isAuthenticated===true ? (<Component{...props}/>):(<Redirect to='/hompage'/>)}/>    
 }
-export default SecureScan;
+export default SecureScan;//I can use this function within a <HashRouter>, use to 'getUserConfirmation'... I suppose, I can use this component for authentication?
