ec0e21ae2e1f23f7cd34774c2087e1f211edbe4e
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-27 11:55:31 -0500

    troubleshooting/version control

--- client/src/store/actions/authActions.js
+++ client/src/store/actions/authActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import setAuthToken from '../../setAuthToken';//jwt-decode comes into play
+import setAuthToken from '../../utils/setAuthToken';//jwt-decode comes into play
 import jwt_decode from 'jwt-decode';//bit coin mining
-import {Get_ERRORS,SET_CURRENT_USER,USER_LOADING} from './types';
+import {GET_ERRORS,SET_CURRENT_USER,USER_LOADING} from './types';
 
 export const setCurrentUser=decoded=>{
     return{
@@ -16,7 +16,7 @@ export const setUserLoading=()=>{
 };
 export const setErrors=err=>{
     return{
-        type:Get_ERRORS,
+        type:GET_ERRORS,
         payload:err.response.data,
     }
 };
@@ -32,7 +32,7 @@ export const loginUser=(userData,history)=>dispatch=>{
     
 
     localStorage.setItem('jwtToken',token);
-    setAuthToken(Token);
+    setAuthToken(token);
     
     const decoded=jwt_decode(token);
     dispatch(setCurrentUser(decoded));
